a0d3abdee243063183ee773c166c915ecd1e4708
Lorenzo De Santis <desantis.1849114@studenti.uniroma1.it>
2023-09-11 12:03:02 +0200

    some fixes

--- scripts/multivariate/ETTh2.sh
+++ scripts/multivariate/ETTh2.sh
@@ -1,4 +1,4 @@
-for pred_len in 24 48 168 336 720
+for pred_len in 24 48 168 336
 do
 python long_range_main.py \
 -data ETTh2 \
@@ -8,3 +8,11 @@ python long_range_main.py \
 -n_head 6 \
 -iter_num 3
 done
+python long_range_main.py \
+-data ETTh2 \
+-data_path ETTh2.csv \
+-input_size 336 \
+-predict_step 720 \
+-inner_size 5 \
+-n_head 6 \
+-iter_num 3
