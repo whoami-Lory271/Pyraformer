a5fe98f692dff3f62af16186e48444e3b7d0a8f0
Lorenzo De Santis <desantis.1849114@studenti.uniroma1.it>
2023-09-13 13:38:18 +0200

    some fixes

--- scripts/multivariate/ETTm1.sh
+++ scripts/multivariate/ETTm1.sh
@@ -1,16 +1,16 @@
 for pred_len in 24 48 96 288 672
 do
-python long_range_main.py \
--data ETTm1 \
--data_path ETTm1.csv \
--input_size 384 \
--predict_step $pred_len \
--window_size [5,5,5] \
--dropout 0.2 \
--n_head 6 \
--d_model 256 \
--d_bottleneck 64 \
--d_k 64 \
--d_v 64 \
--iter_num 3 \
+  python long_range_main.py \
+  -data ETTm1 \
+  -data_path ETTm1.csv \
+  -input_size 384 \
+  -predict_step $pred_len \
+  -window_size [5,5,5] \
+  -dropout 0.2 \
+  -n_head 6 \
+  -d_model 256 \
+  -d_bottleneck 64 \
+  -d_k 64 \
+  -d_v 64 \
+  -iter_num 3
 done
